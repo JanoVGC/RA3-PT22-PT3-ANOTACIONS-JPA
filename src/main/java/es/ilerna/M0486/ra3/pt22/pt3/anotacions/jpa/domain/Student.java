ebca8a0bfddf4ca907865ae23bf294685d3f9d55
jan altisent <altisentbrescojan@alumnes.ilerna.com>
2026-01-26 19:37:16 +0100

    Update Student.java

--- src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/domain/Student.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/domain/Student.java
@@ -2,12 +2,14 @@ package es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain;
 
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("STUDENT")
 public class Student extends Person {
 
-    @Column
+    @Column (name = "studentCode")
     private String studentCode;
 
 
